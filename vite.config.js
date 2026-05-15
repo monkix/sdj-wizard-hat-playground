36981d035c23a9e35ab4d95ff8b0329b4afc68d8
monkix <monkix@users.noreply.github.com>
2026-05-15 15:14:38 +0700

    Use relative asset paths for Pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1];
-
 export default defineConfig({
-  base: repoName ? `/${repoName}/` : "/",
+  base: "./",
   plugins: [react()],
 });
